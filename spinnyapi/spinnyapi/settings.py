141cb0ce5b7cdf3699f302ae398473f007ed4a84
Abhixsmasher <71562641+Abhixsmasher@users.noreply.github.com>
2023-12-17 16:45:49 +0530

    Update settings.py

--- spinnyapi/spinnyapi/settings.py
+++ spinnyapi/spinnyapi/settings.py
@@ -62,6 +62,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 WSGI_APPLICATION = "spinnyapi.wsgi.application"
